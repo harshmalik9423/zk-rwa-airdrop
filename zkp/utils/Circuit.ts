b784bba38e7dcfc930282b9cf734cc1fac571cbf
Anushka Agarwal <agarwalanushka4@gmail.com>
2024-12-18 21:39:01 +0530

    feat: utils

---
+++ zkp/utils/Circuit.ts
@@ -0,0 +1,36 @@
+import { readFileSync } from "fs";
+import { resolve } from "path";
+import { plonk } from "snarkjs";
+
+export class Circuit {
+	circuit: string;
+	wasmPath: string;
+	zkeyPath: string;
+	vkey: any;
+
+	constructor(circuit: string) {
+		this.circuit = circuit;
+		this.vkey = JSON.parse(
+			readFileSync(resolve(__dirname, `../build/circuits/${circuit}/${circuit}.vkey.json`), `utf-8`)
+		);
+		this.zkeyPath = resolve(__dirname, `../build/circuits/${circuit}/${circuit}.zkey`);
+		this.wasmPath = resolve(__dirname, `../build/circuits/${circuit}/${circuit}.wasm`);
+	}
+
+	async generateProof(inputs: any): Promise<any> {
+		try{
+			const { proof, publicSignals } = await plonk.fullProve(inputs, this.wasmPath, this.zkeyPath);
+			let proofCalldata = await plonk.exportSolidityCallData(proof, publicSignals);
+			proofCalldata = proofCalldata.split(",")[0].toString();
+				return { proofJson: proof, proofCalldata: proofCalldata, publicSignals: publicSignals };
+		} catch (error) {
+			console.log("Error while generating Proof : ", error);
+			return { error: "Equity Threshold not met" };
+		}
+	}
+
+	async verifyProof(proofJson: any, publicSignals: any): Promise<boolean> {
+		const verify = await plonk.verify(this.vkey, publicSignals, proofJson);
+		return verify;
+	}
+}
